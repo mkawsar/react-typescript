7b22fbea3dd89ee6e1568be9e36749cbaed87465
mkawsar <kawsar.diu888@gmail.com>
2024-05-23 13:50:18 +0600

    initial project steup

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import {FC, useEffect} from 'react'
+
+interface AppProps {
+    title: string
+}
+
+const App: FC <AppProps> = ({ title }) => {
+    useEffect(() => {
+        const getUsers = async() => {
+            try {
+                const { data } = await axios.get('https://randomuser.me/api/?results=10');
+                console.log(data);
+              } catch (error) {
+                console.log(error);
+              }
+        }
+        getUsers();
+    }, []);
+
+    return <h1>{title}</h1>
+}
+
+export default App;
