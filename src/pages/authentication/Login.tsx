a8d357e237ccf5c81cc4efb4403b5cf0b185c16f
mkawsar <kawsar.diu888@gmail.com>
2024-05-25 19:53:16 +0600

    auth context updated

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -1,5 +1,29 @@
-const Login = () => {
-    return <div>Welcome Login Page</div>
-}
+import type { FC } from 'react';
+import LoginJWt from '../../components/authentication/login';
+import { Box, Card, CardContent, Container, Typography } from '@mui/material';
+
+const Login: FC = () => {
+    return(
+        <Box sx={{ backgroundColor: 'background.default', display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+           <Container maxWidth="sm" sx={{ py: '80px' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'center', mb: 8 }}>
+                    <Card>
+                        <CardContent sx={{ display: 'flex', flexDirection: 'column', p: 4, }}>
+                            <Box sx={{ alignItems: 'center', display: 'flex', justifyContent: 'space-between', mb: 3 }}>
+                                <div>
+                                    <Typography color='textPrimary' gutterBottom variant='h4'>Log in</Typography>
+                                    <Typography color='textSecondary' variant='body2'>Log in on the internal platform</Typography>
+                                </div>
+                            </Box>
+                            <Box>
+                                <LoginJWt />
+                            </Box>
+                        </CardContent>
+                    </Card>
+                </Box>
+           </Container>
+        </Box>
+    );
+};
 
 export default Login;
